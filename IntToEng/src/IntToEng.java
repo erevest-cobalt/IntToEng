9fef7f8783b49dba5a01edcefcbad3bb88992685
yuna-rin-k <g17922ky@gm.tsuda.ac.jp>
2020-06-12 15:17:54 +0900

    first commit

---
+++ IntToEng/src/IntToEng.java
@@ -0,0 +1,15 @@
+
+public class IntToEng {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+    }
+
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+        return "";
+    }
+
+}
